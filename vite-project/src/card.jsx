bc5ca29bcd20b7c8e001214f4c69a9aea1c008ca
jesusre890 <jesusre890@gmail.com>
2023-07-15 16:33:59 -0300

    carta creada jesus

---
+++ vite-project/src/card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <div>carta 1</div>
+      <div>carta 2</div>
+      <div>carta 3</div>
+      <div>carta 4</div>
+      <div>carta 5</div>
+      <div>carta 6</div>
+    </div>
+  );
+};
+
+export default card;
